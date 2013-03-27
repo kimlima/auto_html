ed44a2f689a256c988c559702a3401f238a4029f
Kim Lima <kimlima9@gmail.com>
2013-03-27 19:33:11 -0300

    trying to avoid skip

--- lib/auto_html/filters/youtube_with_start.rb
+++ lib/auto_html/filters/youtube_with_start.rb
@@ -6,21 +6,25 @@ AutoHtml.add_filter(:youtube_with_start).with(:width => 420, :height => 315, :fr
     height = options[:height]
     frameborder = options[:frameborder]
     start_time = 0
+    start_time_embed = 0
+    start_time_short = 0
     sub_text_embed = $4
     sub_text_short = $5
     if !sub_text_embed.blank?
       parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
       sub_text_embed.gsub(parameters) do
-        start_time += 60*$2.to_i if !$2.blank?
-        start_time += $4.to_i if !$4.blank?
-      end
-    elsif !sub_text_short.blank?
-      parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
-      sub_text_short.gsub(parameters) do
-        start_time += 60*$2.to_i if !$2.blank?
-        start_time += $4.to_i if !$4.blank?
+        start_time_embed += 60*$2.to_i if !$2.blank?
+        start_time_embed += $4.to_i if !$4.blank?
       end
     end
+    if !sub_text_short.blank?
+      parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
+      sub_text_short.gsub(parameters) do
+        start_time_short += 60*$2.to_i if !$2.blank?
+        start_time_short += $4.to_i if !$4.blank?
+      end
+    end
+    start_time = (start_time_embed > start_time_short ? start_time_embed : start_time_short)
   	wmode = options[:wmode]
 		src = "//www.youtube.com/embed/#{youtube_id}"
 		src += "?wmode=#{wmode}" if wmode
