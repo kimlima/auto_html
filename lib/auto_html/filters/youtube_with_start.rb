32045e4fb33f67ef1158e19e31e9f6ab0c0cb5de
Kim Lima <kimlima9@gmail.com>
2013-10-10 18:39:49 -0300

    another tweak

--- lib/auto_html/filters/youtube_with_start.rb
+++ lib/auto_html/filters/youtube_with_start.rb
@@ -1,15 +1,15 @@
 AutoHtml.add_filter(:youtube_with_start).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil) do |text, options|
-  regex = /<a href=\"https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=.*v=)([A-Za-z0-9_-]*)(\&\S+)?(\S*)/
+  regex = /(<a href=\")?https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=.*v=)([A-Za-z0-9_-]*)(\&\S+)?(\S*)/
   text.gsub(regex) do
-    youtube_id = $3
+    youtube_id = $4
     width = options[:width]
     height = options[:height]
     frameborder = options[:frameborder]
     start_time = 0
     start_time_embed = 0
     start_time_short = 0
-    sub_text_embed = $4
-    sub_text_short = $5
+    sub_text_embed = $5
+    sub_text_short = $6
     if !sub_text_embed.blank?
       parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
       sub_text_embed.gsub(parameters) do
