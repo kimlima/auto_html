b7b73bb5016e06fe849455153aed17e291678c12
Kim Lima <kimlima9@gmail.com>
2014-03-21 21:07:56 -0300

    avoid errors if it doesn't end with white spaces

--- lib/auto_html/filters/youtube_with_start.rb
+++ lib/auto_html/filters/youtube_with_start.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:youtube_with_start).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil) do |text, options|
-  regex = /(<a href=\")?https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=.*v=)([A-Za-z0-9_-]*)(\&\S+)?(\S*)/
+  regex = /(<a href=\")?https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=.*v=)([A-Za-z0-9_-]*)(([&?#][A-Za-z=0-9_-]*)*)/
   text.gsub(regex) do
     youtube_id = $4
     width = options[:width]
@@ -9,19 +9,11 @@ AutoHtml.add_filter(:youtube_with_start).with(:width => 420, :height => 315, :fr
     start_time_embed = 0
     start_time_short = 0
     sub_text_embed = $5
-    sub_text_short = $6
     if !sub_text_embed.blank?
       parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
       sub_text_embed.gsub(parameters) do
         start_time_embed += 60*$2.to_i if !$2.blank?
         start_time_embed += $4.to_i if !$4.blank?
-      end
-    end
-    if !sub_text_short.blank?
-      parameters = /.*[&?#]t=((\d*)m)?((\d*)s)?/
-      sub_text_short.gsub(parameters) do
-        start_time_short += 60*$2.to_i if !$2.blank?
-        start_time_short += $4.to_i if !$4.blank?
       end
     end
     start_time = (start_time_embed > start_time_short ? start_time_embed : start_time_short)
